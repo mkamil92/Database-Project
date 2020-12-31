98ba59379244655707dfc2c53e1d34902031590a
Mohammad Kamil <76086826+mkamil92@users.noreply.github.com>
2020-12-31 16:56:48 +0500

    upload admin site

---
+++ Admin/index.php
@@ -0,0 +1,41 @@
+<?php
+include('header.php');
+include('connection.php');
+?>
+<!--/.sidebar-->
+<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
+		<div class="row">
+			<ol class="breadcrumb">
+				<li><a href="#">
+					<em class="fa fa-home"></em>
+				</a></li>
+				<li class="active">Dashboard</li>
+			</ol>
+		</div><!--/.row-->
+		
+		<div class="row">
+			<div class="col-lg-12">
+				<h1 class="page-header">Dashboard</h1>
+			</div>
+		</div>
+	<hr style="border:1px solid black;">
+		<table style="list-style-type: none;border:5px solid white;margin-left: 15px;">
+			<tr>
+				<td style="margin: 40px;padding:80px;text-align: center;"><em class="fa fa-power-off fa-users" style="font-size:90px; color: orange">&nbsp;</em>
+				<h3 style="font-weight: bold;color:orange;">Total Users : 55</h3></td>
+
+				<td  style="margin: 40px; padding:80px;text-align: center;"><em class="fa fa-power-off fa-upload" style="font-size:90px;color: green ">&nbsp;</em>
+				<h3><li><a href="upload.php" style="font-weight: bold;color:green;">Click for Products</a></li></h3></td>
+
+				<td style="margin: 40px; padding:80px;text-align: center;"><em class="fa fa-power-off fa-shopping-cart" style="font-size:90px; color: red ">&nbsp;</em>
+				<h3><li><a href="#" style="font-weight: bold;color:red;"> Click for New Orders</a></li></h3></td>
+			</tr>
+		</ul>
+	</table>
+	<hr style="border:1px solid black;">
+
+
+<?php
+include('header.php');
+?>
+	
---
+++ Admin/newmessages.php
@@ -0,0 +1,20 @@
+<?php
+include('header.php');
+$connection = new mysqli("localhost", "root", "", "dbms");
+ 
+// Check connection
+if($connection === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
+
+
+
+
+
+
+
+
+<?php
+include('header.php');
+?>
